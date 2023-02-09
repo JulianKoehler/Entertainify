c2c4d9143e669ecb8319187aa4c47b2f537aad44
Julian Köhler <112172251+JulianKoehler@users.noreply.github.com>
2023-02-09 13:50:39 +0100

    minor changes

--- src/util/toggleBookmark.ts
+++ src/util/toggleBookmark.ts
@@ -2,7 +2,7 @@ import { firebaseConfig } from "../firebase";
 import { MovieOrSeries, Trending } from "../models/moviesAndSeries";
 
 async function toggleBookmark(item: Trending | MovieOrSeries, isBookmarked: boolean) {
-  /* queryEndpoint is just removing the .json from the url so that I can put the id inbetween when sending the Patch request */
+  /* dbEndpoint is just removing the .json from the url so that I can put the id inbetween when sending the Patch request */
   const dbEndpoint = firebaseConfig.dbAll.slice(0, firebaseConfig.dbAll.length - 5);
 
   try {
